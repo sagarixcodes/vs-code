18586bacff3ba0669578d8d601f6271d319686d6
sagarix <soumyashende2011@gmail.com>
2026-05-12 10:24:23 +0530

    updated with more user interaction

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,6 +1,13 @@
-import random
+print("**WELCOME TO WORLDS BEST PASSWORD GENERATOR**")
+print("Press enter to use our service!")
+input()
 
-def password_generator(length):
+while True:
+  import random
+  import keyboard
+
+
+  def password_generator(length):  
     codes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
 
     password = ""
@@ -10,17 +17,27 @@ def password_generator(length):
 
     return password    
 
-print("**WELCOME TO WORLDS BEST PASSWORD GENERATOR**")
-length = int(input("Enter the desired length of PASSWORD you want to generate : "))
 
-with open("created_password","a+") as cp :
+  length = int(input("Enter the desired length of PASSWORD you want to generate : "))
+
+  with open("created_password","a+") as cp :  
     created_passwords = cp.read()
 
-password = ""
-while password  in created_passwords:
-    password = password_generator(length)
+  password = ""
+  while password  in created_passwords:
+      password = password_generator(length)
 
-with open("created_password","a+") as cp :
+  with open("created_password","a+") as cp :
     cp.write(password + "\n")
     
-print("Your password is:",password)
+  print("Your password is:",password,"\n")
+  print("Do you want to regenerate a new password ?")
+  print("Press enter to do so or press q to stop")
+  user_input = keyboard.read_key()
+
+  if user_input == "enter":
+     continue
+  
+  elif user_input == "q":
+     print("THANK YOU!for using our service.")
+     break
