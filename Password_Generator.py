203f71ed4eac2bdfc2f88333f5b8594d0b95a3e0
sagarix <soumyashende2011@gmail.com>
2026-05-14 11:13:51 +0530

    Updated with error handeling

--- Password_Generator.py
+++ Password_Generator.py
@@ -2,42 +2,46 @@ print("**WELCOME TO WORLDS BEST PASSWORD GENERATOR**")
 print("Press enter to use our service!")
 input()
 
-while True:
-  import random
-  import keyboard
+import random
+import keyboard
 
-
-  def password_generator(length):  
+def password_generator(length):
     codes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&"
-
     password = ""
 
     for i in range(length):
         password += random.choice(codes)
 
-    return password    
+    return password
 
 
-  length = int(input("Enter the desired length of PASSWORD you want to generate : "))
+while True:
+    try:
+        length = int(input("Enter the desired length of PASSWORD you want to generate: "))
 
-  with open("created_password","a+") as cp :  
-    created_passwords = cp.read()
+        with open("created_password", "a+") as cp:
+            cp.seek(0)
+            created_passwords = cp.read()
 
-  password = ""
-  while password  in created_passwords:
-      password = password_generator(length)
+        password = ""
+        while password in created_passwords:
+            password = password_generator(length)
 
-  with open("created_password","a+") as cp :
-    cp.write(password + "\n")
-    
-  print("Your password is:",password,"\n")
-  print("Do you want to regenerate a new password ?")
-  print("Press enter to do so or press q to stop")
-  user_input = keyboard.read_key()
+        with open("created_password", "a+") as cp:
+            cp.write(password + "\n")
 
-  if user_input == "enter":
-     continue
-  
-  elif user_input == "q":
-     print("THANK YOU!for using our service.")
-     break
+        print("Your password is:", password)
+        print("Do you want to regenerate a new password?")
+        print("Press ENTER to do so or press ESC to stop")
+
+        user_input = keyboard.read_key()
+
+        if user_input == "enter":
+            continue
+
+        elif user_input == "esc":
+            print("THANK YOU for using our service.")
+            break
+
+    except ValueError:
+        print("Invalid input.Try again!")
